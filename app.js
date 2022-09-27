71769caf8d1c4741541a71adf6e9957e2932f69a
Valerie K <valerik210695@gmail.com>
2022-09-27 21:00:56 +0300

    create new app

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+async function main() {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/mestodb', {
+      useNewUrlParser: true,
+      useUnifiedTopology: false,
+    });
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  }
+
+  try {
+    await app.listen(PORT);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  }
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+}
+
+main();
